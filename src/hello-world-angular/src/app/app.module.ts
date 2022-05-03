2e2062bb572cf750d4e7d4dba1e08615d6b44736
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2022-05-03 07:45:37 +0200

    Add server component

--- src/hello-world-angular/src/app/app.module.ts
+++ src/hello-world-angular/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Root App Component (1 tipical for the app)
 })
 export class AppModule { }
