94155e6d3a34516bcd3c391bf52a39ce02bcb55b
Matteo Rebecchi Mari <dev.matteo.rebecchimari@gmail.com>
2022-05-07 14:04:49 +0200

    added password visibility behavior

--- src/hello-world-angular/src/app/login-page/login-page.component.ts
+++ src/hello-world-angular/src/app/login-page/login-page.component.ts
@@ -14,6 +14,11 @@ export class LoginPageComponent implements OnInit {
 
   // This property use Binding [(ngModel)]="password"
   password: String = "";
+  isPasswordVisible = false;
+
+  inputPasswordType(): String {
+    return this.isPasswordVisible ? "text" : "password";
+  }
 
   constructor() { }
 
@@ -28,16 +33,26 @@ export class LoginPageComponent implements OnInit {
     this._updateUI();
   }
 
+  // enable/disable login button
   _updateUI(): void {
     this.isLoginEnabled = !this._isEmptyFunction(this.username) && !this._isEmptyFunction(this.password);
   }
 
+  // check empty string
   _isEmptyFunction(stringValue: String): boolean {
     return (!stringValue || stringValue.length === 0);
   }
 
+  // Toggle password visibility
+  onPasswordVisiblityChanged() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  // Login button pressed
   onLogin() {
     console.log("Login pressed");
+    this.username = '';
+    this.password = '';
   }
 
 }
